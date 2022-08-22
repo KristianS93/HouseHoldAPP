39453dcb392e846ea3f08c1d54d596a9f09e6b32
Kristian Sørensen <82061735+KristianS93@users.noreply.github.com>
2022-08-22 10:31:21 +0200

    Update mongoclient.go

--- GroceryList/database/mongoclient.go
+++ GroceryList/database/mongoclient.go
@@ -12,6 +12,7 @@ type MongClient struct {
 	Connection *mongo.Collection
 }
 
+//DbConnect has to be called after instantiating a new connection to establish the connection to mongo
 func (m *MongClient) DbConnect(Collection string) {
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(ConstDbURI))
 	if err != nil {
