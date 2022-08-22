2d392eef0ffb62f278bf5ccdb874967ac8ff9784
Andreas Krath <krathermand@gmail.com>
2022-08-22 23:47:40 +0200

    starting

--- UserSystem/service/service.go
+++ UserSystem/service/service.go
@@ -1,12 +1,39 @@
 package service
 
-import "github.com/gorilla/mux"
+import (
+	"database/sql"
 
-type Server struct {
+	"github.com/gorilla/mux"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Service struct {
 	Router   *mux.Router
 	HostName string
 	HostPort string
+	DB       *sql.DB
+}
 
-	// db connection
-	DB string
+func (s *Service) Init() {
+	if s.Router == nil {
+		s.Router = mux.NewRouter()
+		s.Routes(s.Router)
+	}
+
+	if s.HostName == "" {
+		s.HostName = "localhost"
+	}
+
+	if s.HostPort == "" {
+		s.HostPort = "5001"
+	}
+
+	if s.DB == nil {
+		// implement
+	}
+
+}
+
+func (s *Service) Routes(r *mux.Router) {
+
 }
