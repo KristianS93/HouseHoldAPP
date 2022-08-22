813dd2bde7274bf2d455bf365490ffe1bc52a160
Kristian Sørensen <82061735+KristianS93@users.noreply.github.com>
2022-08-22 10:29:21 +0200

    Update errorassistant.go

--- GroceryList/Service/assistants/errorassistant.go
+++ GroceryList/Service/assistants/errorassistant.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 )
 
+//WrongMethod Check the method from the r pointer and the submitted method, returns a booleans, if true the method is WRONG
 func WrongMethod(r *http.Request, w *http.ResponseWriter, method string) bool {
 	returnVal := false
 	if r.Method != method {
