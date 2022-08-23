5c07684c49cefbcb30b58cf054a566312ccb0cfd
Kristian Sørensen <82061735+KristianS93@users.noreply.github.com>
2022-08-23 12:08:05 +0200

    Update router.go

--- MealPlanner/service/router.go
+++ MealPlanner/service/router.go
@@ -3,5 +3,17 @@ package service
 //Routes handles the routing/handling of endpoints for the service
 // The function is a method for the Server struct, and takes a pointer to a http.servemux type
 func (s *Server) Routes() {
-	s.Router.HandleFunc("/CreateMeal", s.ICreateMeal).Methods("GET")
+	//Meal endpoints
+	s.Router.HandleFunc("/CreateMeal", s.ICreateMeal).Methods("POST")
+	s.Router.HandleFunc("/GetMeal", s.IGetMeal).Methods("GET")
+	s.Router.HandleFunc("/ChangeMeal", s.IChangeMeal).Methods("PATCH")
+	s.Router.HandleFunc("/DeleteMeal", s.ICreateMeal).Methods("DELETE")
+	
+	//Plan endpoints
+	s.Router.HandleFunc("/CreatePlan", s.ICreatePlan).Methods("POST")
+	s.Router.HandleFunc("/GetPlan", s.IGetPlan).Methods("GET")
+	s.Router.HandleFunc("/ChangePlan", s.IChangePlan).Methods("PATCH")
+	s.Router.HandleFunc("/GeneratePlan", s.IGeneratePlan).Methods("POST") //måske get med url param
+	s.Router.HandleFunc("/GenerateList", s.IGenerateList).Methods("POST")
+	s.Router.HandleFunc("/DeletePlan", s.IDeletePlan).Methods("DELETE")
 }
