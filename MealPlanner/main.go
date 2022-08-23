51d9626e2cf53ba0489f9b0692e9d37c2f644f6f
Kristian Desktop <KristianS93@hotmail.com>
2022-08-23 23:35:38 +0200

    yikes postgres er op af bakke

--- MealPlanner/main.go
+++ MealPlanner/main.go
@@ -1,11 +1,16 @@
 package main
 
-import "mealplanner/service"
+import (
+	"mealplanner/service"
+	"mealplanner/service/meal"
+)
 
 func main() {
 	//initialize server from server/web package
 	Server := service.Server{}
-
+	meal.DBConnection()
+	// meal.DBConnection()
+	// meal.DB.AutoMigrate(meal.User{})
 	//Initialize server settings
 	Server.Init()
 	//Run server/service instance.
---
+++ MealPlanner/service/meal/config.go
@@ -0,0 +1,21 @@
+package meal
+
+import (
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+var DSN = "host=localhost user=kris password=mypass dbname=gorm port=27021"
+
+func DBConnection() {
+	var error error
+	DB, error = gorm.Open(postgres.Open(DSN), &gorm.Config{})
+	if error != nil {
+		log.Fatal(error)
+	} else {
+		log.Println("Database connection successful")
+	}
+}
