50aa7ccd1998394d8f0fae219beeab4517316149
Kristian Desktop <KristianS93@hotmail.com>
2022-08-25 21:08:07 +0200

    changes

--- MealPlanner/database/config.go
+++ MealPlanner/database/config.go
@@ -3,7 +3,10 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
@@ -16,18 +19,32 @@ import (
 //	dbname   = "postgres"
 //
 // )
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "postgres"
-	dbname   = "postgres"
-)
+
+// const (
+// 	host     = "localhost"
+// 	port     = 5432
+// 	user     = "MpAdmin"
+// 	password = "MpPass"
+// 	dbname   = "Mp"
+// )
 
 func Connect() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+	err := godotenv.Load("./database/.env")
+	if err != nil {
+		log.Fatal("Error loading env.")
+	}
+	usr := os.Getenv("POSTGRES_USER")
+	pw := os.Getenv("POSTGRES_PASSWORD")
+	dbName := os.Getenv("POSTGRES_DB")
+	host := os.Getenv("MP_HOST")
+	port := os.Getenv("MP_PORT")
+
+	// psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+	// 	"password=%s dbname=%s sslmode=disable",
+	// 	host, port, user, password, dbname)'
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
 		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
+		host, port, usr, pw, dbName)
 
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
