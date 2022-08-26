bad77e77ce3d30062789ea8f0cf1a248bf09af3e
Kristian Sørensen <82061735+KristianS93@users.noreply.github.com>
2022-08-26 11:33:26 +0200

    Delete MP.session.sql

--- MealPlanner/MP.session.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS meal(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    description TEXT,
-    items TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
