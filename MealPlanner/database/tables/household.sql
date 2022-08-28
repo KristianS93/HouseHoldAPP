bbbcb04e46e66a1293c061d173390f7e7d598b1e
Kristian Desktop <KristianS93@hotmail.com>
2022-08-28 23:47:07 +0200

    added deletemeal

--- MealPlanner/database/tables/household.sql
+++ MealPlanner/database/tables/household.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS household (
     id SERIAL PRIMARY KEY,
     householdid VARCHAR(100) UNIQUE,
-    meals int[],
-    grocerylist VARCHAR(100)
+    grocerylist VARCHAR(100) UNIQUE,
+    plans int [],
+    meals int []
 );
