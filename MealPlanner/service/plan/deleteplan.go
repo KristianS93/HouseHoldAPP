6d88d2bd8adec7ec7cb328d6f09cd312eea50ade
Kristian Sørensen <82061735+KristianS93@users.noreply.github.com>
2022-08-30 12:07:51 +0200

    Update deleteplan.go

--- MealPlanner/service/plan/deleteplan.go
+++ MealPlanner/service/plan/deleteplan.go
@@ -6,6 +6,33 @@ import (
 )
 
 func DeletePlan(w http.ResponseWriter, r *http.Request) {
-	// Delete plan skal slette planen fra plan db og fra household, det er ikke nødvendigt at slette meals fra planen, da meals kan stadig være relevante.
-	fmt.Println("Noget med en plan")
+	db := database.Connect()
+	defer db.Con.Close()
+	//Enable cors and set header to return json
+	assistants.EnableCors(&w)
+	assistants.SetHeader(&w)
+	
+	//Decode planid
+	var getPlanId models.PlanId
+	err := assistants.DecodeData(r, &getPlanId)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		io.WriteString(w, `{"Error": "Bad request: Getting data"}`)
+		return
+	}
+	
+	//Check that plan id is not empty
+	if getPlanId.PlanId == 0 || getPlanId.HouseholdId == "" {
+		log.Println("Plan or household id missing")
+		w.WriteHeader(http.StatusBadRequest)
+		io.WriteString(w, `{"Error": "Bad request: Missing household id or week number"}`)
+		return
+	}
+	
+	//Update household plans
+	
+	//Delete plan from plan db
+	
+	//Return Succes	
 }
