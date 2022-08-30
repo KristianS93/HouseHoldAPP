2e237149eb1e9e7ef74e05e1fce047f4d1e3acd0
Kristian Sørensen <82061735+KristianS93@users.noreply.github.com>
2022-08-30 11:18:30 +0200

    Update deleteplan.go

--- MealPlanner/service/plan/deleteplan.go
+++ MealPlanner/service/plan/deleteplan.go
@@ -6,5 +6,6 @@ import (
 )
 
 func DeletePlan(w http.ResponseWriter, r *http.Request) {
+	// Delete plan skal slette planen fra plan db og fra household, det er ikke nødvendigt at slette meals fra planen, da meals kan stadig være relevante.
 	fmt.Println("Noget med en plan")
 }
