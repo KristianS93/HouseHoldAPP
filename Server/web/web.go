021d345312f14b6304791e20000dea078e94af8e
Andreas Krath <krathermand@gmail.com>
2022-10-14 00:01:01 +0200

    added favicon handler

--- Server/web/web.go
+++ Server/web/web.go
@@ -2,6 +2,7 @@ package web
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/favicon"
 	"github.com/gofiber/template/html"
 )
 
@@ -16,18 +17,28 @@ func GetApp() *fiber.App {
 		EnablePrintRoutes: true,
 		ErrorHandler:      ErrorHandler,
 	})
+
+	// statically served files
 	app.Static("/", "./public/static")
 
+	// Middleware
+	app.Use(favicon.New(favicon.Config{
+		File: "./public/static/images/favicon.ico",
+	}))
+
+	// GET endpoints
 	app.Get("/", Index)
 	app.Get("/grocerylist", GroceryList)
 
+	// POST endpoints
 	app.Post("/additem", Additem)
 	app.Post("login", Login)
 
+	// PATCH endpoints
 	app.Patch("/changeitem", ChangeItem)
 
 	// should be changed to fetch instead of hyperlink frontend, should also change to delete method
 	app.Get("/clearlist", ClearList)
-	
+
 	return app
 }
